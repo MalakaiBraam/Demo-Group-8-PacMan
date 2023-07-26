6a96d55222a191afbf284b032dec3f926c5aab16
MalakaiBraam <140538653+MalakaiBraam@users.noreply.github.com>
2023-07-26 15:55:00 +0200

    CloneTest

---
+++ Game-Deisgn-Assignment-1 - Copy/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    NavMeshAgent agent;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(rb.velocity.y);
+        Vector2 CombinedMove = new Vector2(moveX, moveY);
+        Vector2 NormalizedVelo = CombinedMove.normalized;
+        
+
+        // Move the player horizontally and vertically
+        //rb.velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+        rb.velocity = new Vector2(CombinedMove.x,CombinedMove.y)*moveSpeed;
+
+        if (rb.velocity.x == 0)
+        {
+            rb.velocity = new Vector2(CombinedMove.x +0.0001f, CombinedMove.y) * moveSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //agent.nextPosition = transform.position;
+    }
+}
