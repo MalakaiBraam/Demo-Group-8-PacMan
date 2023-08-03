117f138481fcd155e8a60d95738cc0cc9f6d5eb9
TameraDaniel <2424818@students.wits.ac.za>
2023-08-04 00:24:17 +0200

    completed game

--- Game-Deisgn-Assignment-1 - Copy/Assets/Script/GameManager.cs
+++ Game-Deisgn-Assignment-1 - Copy/Assets/Script/GameManager.cs
@@ -8,12 +8,19 @@ public class GameManager : MonoBehaviour
     public GameObject pacman;
     public GameObject leftWarpNode;
     public GameObject rightWarpNode;
+
     public AudioSource siren;
     public AudioSource munch1;
     public AudioSource munch2;
+    public AudioSource powerPelletAudio;
+    public AudioSource respawningAudio;
+    public AudioSource ghostEatenAudio;
+
     public int currentMunch = 0;
+
     public int Score;
     public Text scoreText;
+
     public GameObject ghostNodeLeft;
     public GameObject ghostNodeRight;
     public GameObject ghostNodeCentre;
@@ -24,7 +31,36 @@ public class GameManager : MonoBehaviour
     public GameObject blueGhost;
     public GameObject orangeGhost;
 
+    public EnemyControllerTester redGhostController;
+    public EnemyControllerTester pinkGhostController;
+    public EnemyControllerTester blueGhostController;
+    public EnemyControllerTester orangeGhostController;
+
     public bool hadDeathOnThisLevel = false;
+
+    public bool gameIsRunning;
+
+    public List<NodeController> nodeControllers = new List<NodeController>();
+
+    public bool newGame;
+    public bool clearedLevel;
+
+    public Image blackBackground;
+
+    public Text gameOverText;
+    public Text livesText;
+
+    public bool isPowerPelletRunning = false;
+    public float currentPowerPelletTime = 0;
+    public float powerPelletTimer = 8f;
+    public int powerPelletMultiplyer = 1;
+
+    public AudioSource startGameAudio;
+    public AudioSource death;
+
+    public int lives;
+    public int currentLevel;
+
     public enum GhostMode
     {
         chase, scatter
@@ -32,29 +68,187 @@ public class GameManager : MonoBehaviour
 
     public GhostMode currentGhostMode;
 
+    public int[] ghostModeTimers = new int[] { 7, 20, 7, 20, 5, 20, 5 };
+    public int ghostModeTimerIndex;
+    public float ghostModeTimer = 0;
+    public bool runningTimer;
+    public bool completedTimer;
+
     public int totalPellets;
     public int pelletsLeft;
     public int pelletsCollectedOnThisLife;
+
+  
+
     // Start is called before the first frame update
     void Awake()
     {
-        pinkGhost.GetComponent<EnemyControllerTester>().readyToLeaveHome = true;
-        currentGhostMode = GhostMode.chase;
-        ghostNodeStart.GetComponent<NodeController>().isGhostStartingNode = true;
-       Score = 0;
-       currentMunch = 0;
-       siren.Play();
+        newGame = true;
+        clearedLevel = false;
+        blackBackground.enabled = false;
 
+        redGhostController = redGhost.GetComponent<EnemyControllerTester>();
+        pinkGhostController = pinkGhost.GetComponent<EnemyControllerTester>();
+        blueGhostController = blueGhost.GetComponent<EnemyControllerTester>();
+        orangeGhostController = orangeGhost.GetComponent<EnemyControllerTester>();
+      
+       ghostNodeStart.GetComponent<NodeController>().isGhostStartingNode = true;
+
+        pacman = GameObject.Find("Player");
+
+        StartCoroutine(Setup());
+
+    }
+
+    public IEnumerator Setup()
+    {
+        ghostModeTimerIndex = 0;
+        ghostModeTimer = 0;
+        completedTimer = false;
+        runningTimer = true;
+        gameOverText.enabled = false;
+        //if pacman clears a level, a background will appear covering the level, and the game will pause for 0.1 seconds
+        if(clearedLevel)
+        {
+            blackBackground.enabled = true;
+            //Activate background
+            yield return new WaitForSeconds(0.1f);
+        }
+        blackBackground.enabled = false;
+
+        pelletsCollectedOnThisLife = 0;
+        currentGhostMode = GhostMode.scatter;
+        gameIsRunning = false;
+        currentMunch = 0;
+
+        float waitTimer = 1f;
+
+        if (clearedLevel || newGame)
+        {
+            pelletsLeft = totalPellets;
+            waitTimer = 4f;
+            //Pellet will respawn when pacman clears level or starts a new game
+            for (int i = 0; i < nodeControllers.Count; i++)
+            {
+                nodeControllers[i].RespawnPellet();
+            }
+
+        }
+
+        if(newGame)
+        {
+            startGameAudio.Play();
+            Score = 0;
+            scoreText.text = "Score: " + Score.ToString();
+            SetLives(3);
+            currentLevel = 1;
+        }
+
+
+        pacman.GetComponent<PlayerController>().Setup();
+
+        redGhostController.Setup();
+        pinkGhostController.Setup();
+        blueGhostController.Setup();
+        orangeGhostController.Setup();
+
+        newGame = false;
+        clearedLevel = false;
+        yield return new WaitForSeconds(waitTimer);
+
+        StartGame();
+
+    }
+
+    void SetLives(int newLives)
+    {
+        lives = newLives;
+        livesText.text = "Lives: " + lives;
+    }
+
+    void StartGame()
+    {
+        gameIsRunning = true;
+        siren.Play();
+    }
+    
+    void StopGame()
+    {
+        gameIsRunning = false;
+        siren.Stop();
+        powerPelletAudio.Stop();
+        respawningAudio.Stop();
+        pacman.GetComponent<PlayerController>().Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!gameIsRunning)
+        {
+            return;
+        }
+
+        if (redGhostController.ghostNodeState == EnemyControllerTester.GhostNodesStatesEnum.respawning
+            || pinkGhostController.ghostNodeState == EnemyControllerTester.GhostNodesStatesEnum.respawning
+            || blueGhostController.ghostNodeState == EnemyControllerTester.GhostNodesStatesEnum.respawning
+            || orangeGhostController.ghostNodeState == EnemyControllerTester.GhostNodesStatesEnum.respawning)
+        {
+            if (!respawningAudio.isPlaying)
+            {
+                respawningAudio.Play();
+            }
+        }
+        else
+        {
+            if(respawningAudio.isPlaying)
+            {
+                respawningAudio.Stop();
+            }
+        }
+       
+        if(!completedTimer && runningTimer)
+        {
+            ghostModeTimer += Time.deltaTime;
+            if(ghostModeTimer >= ghostModeTimers[ghostModeTimerIndex])
+            {
+                ghostModeTimer = 0;
+                ghostModeTimerIndex++;
+                if(currentGhostMode == GhostMode.chase)
+                {
+                    currentGhostMode = GhostMode.scatter;
+                }
+                else
+                {
+                    currentGhostMode = GhostMode.chase;
+                }
+
+                if(ghostModeTimerIndex == ghostModeTimers.Length)
+                {
+                    completedTimer = true;
+                    runningTimer = false;
+                    currentGhostMode = GhostMode.chase;
+                }
+            }
+        }
+
+        if (isPowerPelletRunning)
+        {
+            currentPowerPelletTime += Time.deltaTime;
+            if (currentPowerPelletTime >= powerPelletTimer)
+            {
+                isPowerPelletRunning = false;
+                currentPowerPelletTime = 0;
+                powerPelletAudio.Stop();
+                siren.Play();
+                powerPelletMultiplyer = 1;
+            }
+        }
     }
 
-    public void GotPelletFromNodeController()
+    public void GotPelletFromNodeController(NodeController nodeController)
     {
+        nodeControllers.Add(nodeController);
         totalPellets++;
         pelletsLeft++;
     }
@@ -65,7 +259,7 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + Score.ToString();
     }
 
-    public void CollectedPallet(NodeController nodeController)
+    public IEnumerator CollectedPellet(NodeController nodeController)
     {
         //cjecks if the currentmuch is equal to 0 and if it is, it plays the first munch sound
         if(currentMunch == 0)
@@ -105,13 +299,71 @@ public class GameManager : MonoBehaviour
         }
 
         AddToScore(10);
+
+        if(pelletsLeft == 0)
+        {
+            currentLevel++;
+            clearedLevel = true;
+            StopGame();
+            yield return new WaitForSeconds(1);
+            StartCoroutine(Setup());
+        }
+
+        if(nodeController.isPowerPellet)
+        {
+            siren.Stop();
+            powerPelletAudio.Play();
+            isPowerPelletRunning = true;
+            currentPowerPelletTime = 0;
+            powerPelletMultiplyer += 1;
+
+            redGhostController.SetFrightened(true);
+            pinkGhostController.SetFrightened(true);
+            blueGhostController.SetFrightened(true);
+            orangeGhostController.SetFrightened(true);
+        }
     }
 
-    //Add to our score
+    public IEnumerator PauseGame(float timeToPause)
+    {
+        gameIsRunning = false;
+        yield return new WaitForSeconds(timeToPause);
+        gameIsRunning = true;
+    }
+public void GhostEaten()
+    {
+        ghostEatenAudio.Play();
+        AddToScore(400 * powerPelletMultiplyer);
+        powerPelletMultiplyer++;
+        StartCoroutine(PauseGame(1));
+    }
+    
 
-    //Check if there are any pallets left
+    public IEnumerator PlayerEaten()
+    {
+        hadDeathOnThisLevel = true;
+        StopGame();
+        yield return new WaitForSeconds(1);
 
-    //check how many pallets were eaten
+        redGhostController.SetVisible(false);
+        pinkGhostController.SetVisible(false);
+        blueGhostController.SetVisible(false);
+        orangeGhostController.SetVisible(false);
 
-    //is this a power pallet
+        pacman.GetComponent<PlayerController>().Death();
+        death.Play();
+        yield return new WaitForSeconds(3);
+        SetLives(lives - 1);
+        if(lives <= 0)
+        {
+            newGame = true;
+            //Display gameover text
+            gameOverText.enabled = true;
+
+            yield return new WaitForSeconds(3);
+        }
+
+        StartCoroutine(Setup());
+    }
+    
 }
--- Game-Deisgn-Assignment-1 - Copy/Assets/Script/PlayerController.cs
+++ Game-Deisgn-Assignment-1 - Copy/Assets/Script/PlayerController.cs
@@ -6,16 +6,63 @@ public class PlayerController : MonoBehaviour
 {
     MovementController movementController;
 
+    public SpriteRenderer sprite;
+    public Animator animator;
+
+    public GameObject startNode;
+
+    public Vector2 startPos;
+
+    public GameManager gameManager;
+
+    public bool isDead = false;
+
     // Start is called before the first frame update
     void Awake()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        startPos = new Vector2(-0.06f, -0.67f);
+        animator = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
         movementController = GetComponent<MovementController>();
+        startNode = movementController.currentNode;
+    }
+
+    public void Setup()
+    {
+        isDead = false;
+        animator.SetBool("dead", false);
+        animator.SetBool("moving", false);
+        movementController.currentNode = startNode;
+        movementController.direction = "left";
         movementController.lastMovingDirection = "left";
+        sprite.flipX = false;
+        transform.position = startPos;
+        animator.speed = 1;
+       
+    }
+
+    public void Stop()
+    {
+        animator.speed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!gameManager.gameIsRunning)
+        {
+            if(!isDead)
+            {
+                animator.speed = 0;
+            }
+       
+            return;
+        }
+
+        animator.speed = 0;
+
+        animator.SetBool("moving", true);
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             movementController.SetDirection("left");
@@ -32,5 +79,41 @@ public class PlayerController : MonoBehaviour
         {
             movementController.SetDirection("down");
         }
+
+
+        bool flipX = false;
+        bool flipY = false;
+        if (movementController.lastMovingDirection == "left")
+        {
+            animator.SetInteger("direction", 0);
+        }
+        else if(movementController.lastMovingDirection == "right")
+        {
+            animator.SetInteger("direction", 0);
+            flipX = true;
+        }
+        else if(movementController.lastMovingDirection == "up")
+        {
+            animator.SetInteger("direction", 1);
+        }
+        else if (movementController.lastMovingDirection == "down")
+        {
+            animator.SetInteger("direction", 1);
+            flipY = true;
+        }
+
+        sprite.flipY = flipY;
+        sprite.flipX = flipX;
+
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        animator.SetBool("moving", false);
+        animator.speed = 1;
+        animator.SetBool("dead", true);
+       
     }
 }
+
